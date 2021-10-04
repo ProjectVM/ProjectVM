f2d32019bcaf89286012b570e9710701d0386632
neelpatel1028 <47678526+neelpatel1028@users.noreply.github.com>
2021-10-04 15:56:33 -0400

    solving merge conflict

--- server.py
+++ server.py
@@ -9,14 +9,9 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
-<<<<<<< HEAD
-@app.route('/login', methods=['GET'])
-def login():
-=======
 @app.route("/", defaults={'path':''})
 @cross_origin()
 def serve(path):
->>>>>>> 73bfacac90f0a5eb3b03bbe2bd8c45fd9b2d0578
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
