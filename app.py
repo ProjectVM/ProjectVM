da3f11185ef63599e40d12620179f841cac0891f
Vinci Wu <wu.vinci00@gmail.com>
2021-10-29 20:24:33 -0400

    #70 Added checks for valid file type in app.py
    Added check to determine if uploaded file is of mp3 or wav format. Additionally, reformatted app.py to follow Python standard

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
 from flask import Flask, send_from_directory, request, redirect, url_for
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS, cross_origin #comment this on deployment
+from flask_cors import CORS, cross_origin  # comment this on deployment
+from werkzeug.utils import secure_filename
+
 from database import *
 from functions import *
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 CORS(app)
+
 
 @app.route('/')
 @cross_origin()
@@ -18,27 +19,29 @@ def serve():
 @cross_origin()
 def index():
     return {
-        "MSG" : "400"
+        "MSG": "400"
     }
+
 
 @app.route('/register_user', methods=['POST'])
 def register():
     account_information = request.json
     email = account_information['email']
-    username= account_information['username']
+    username = account_information['username']
     passw = account_information['password']
-    print(email,username,passw)
+    print(email, username, passw)
 
-     #checks username is in database and if the password is strong.
+    # checks username is in database and if the password is strong.
     if len(list(get_acc_with_username(username))) == 0 and check_password_strength(passw):
-        insert = {"email": email, "username": username,"password": encrypt_passw(passw)}
+        insert = {"email": email, "username": username, "password": encrypt_passw(passw)}
         add_acc(insert)
         return {
             "MSG": "200"
         }
     return {
-         "MSG": "400"
-     }
+        "MSG": "400"
+    }
+
 
 @app.route('/login_user', methods=['POST'])
 def login():
@@ -48,21 +51,35 @@ def login():
     username = login_info['username']
     password = login_info['password']
     account = list(get_acc_with_username(username))
-    if(len(account) != 0 and check_hash(password,account[0]['password'])):
+    if (len(account) != 0 and check_hash(password, account[0]['password'])):
         print("valid")
         return {
             "MSG": "200"
         }
-    return{
-            "MSG": "400"
+    return {
+        "MSG": "400"
     }
+
+
+# https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
+ALLOWED_EXTENSIONS = {'mp3', 'wav'}
+
+
+# function to check if file is allowed
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/upload_audio', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
+        if upload_file and allowed_file(upload_file.filename):
+            filename = secure_filename(upload_file.filename)
+            return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run()
-
